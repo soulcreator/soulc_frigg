4244755ae47004cf150d8ff694313805aea664a5
soulc <42525568+soulcreator@users.noreply.github.com>
2024-04-09 13:56:48 +0800

    Delete unimall-master directory

--- unimall-master/unimall-app/config/index.js
+++
@@ -1,9 +0,0 @@
-let ENV_CONFIG
-if (process.env.NODE_ENV === 'development') {
-  // 开发环境
-  ENV_CONFIG = require('./.env.dev.js')
-} else {
-  // 生产环境
-  ENV_CONFIG = require('./.env.prod.js')
-}
-module.exports = ENV_CONFIG
--- unimall-master/unimall-runner/src/test/java/com/iotechn/dobbinarchetype/unimall/runner/DemoRunnerApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.iotechn.dobbinarchetype.unimall.runner;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DemoRunnerApplicationTests {
-
-
-}
